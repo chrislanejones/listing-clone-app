a51cdcb8f61176421f3fbd0a414e7ffcc3dadcf0
Chris Lane Jones <chrislanejones@gmail.com>
2024-08-05 23:56:21 -0400

    API with JSON Stringify

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,36 @@ import SearchBar from "./componates/SearchBar";
 // "https://api.hygraph.com/v1/properties"
 const getProperties = async () => {
   const HYGRAPH_ENDPOINT = process.env.HYGRAPH_ENDPOINT;
+  if (!HYGRAPH_ENDPOINT) {
+    throw new error("HYGRAPH_ENDPOINT is not set");
+  }
+  const response = await fetch(HYGRAPH_ENDPOINT, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+      query MyQuery {
+      properties {
+      beds
+      description
+      images {
+        fileName
+        url
+      }
+        location {
+        latitude
+        longitude
+        }
+        name
+        rentalPrice
+        slug
+        id
+      }
+      }`,
+    }),
+  });
 };
 
 const Home = () => {
