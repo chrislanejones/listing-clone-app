b2a5a6ee4e98e56952a071f20a355fa56bcb5db3
Chris Lane Jones <chrislanejones@gmail.com>
2024-08-06 23:53:29 -0400

    Setup NextJS Link with Slug

--- src/app/componates/Card.js
+++ src/app/componates/Card.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
-const Card = () => {
-  return <div></div>;
+const Card = (propertyName, slug, rentalPrice, beds, image) => {
+  return (
+    <Link href={`/property/${slug}`}>
+      <div>{propertyName}</div>
+    </Link>
+  );
 };
 
 export default Card;
---
+++ src/app/componates/property/[slug]/page.js
@@ -0,0 +1,5 @@
+const Property = () => {
+  return <div>I am a property</div>;
+};
+
+export default Property;
