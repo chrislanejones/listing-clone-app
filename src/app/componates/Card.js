b83debb5941f20f6e08576eb9d2b6fa3d347e12b
Chris Lane Jones <chrislanejones@gmail.com>
2024-08-06 23:59:41 -0400

    Card Componate Setup

--- src/app/componates/Card.js
+++ src/app/componates/Card.js
@@ -1,10 +1,18 @@
 import Link from "next/link";
 import React from "react";
+import ImageCard from "./ImageCard";
 
 const Card = (propertyName, slug, rentalPrice, beds, image) => {
   return (
     <Link href={`/property/${slug}`}>
-      <div>{propertyName}</div>
+      <div className="card">
+        <ImageCard />
+        <div className="text-container">
+          <h3>${rentalPrice} / month</h3>
+          <h3>${beds} Beds</h3>
+          <p>{propertyName}</p>
+        </div>
+      </div>
     </Link>
   );
 };
